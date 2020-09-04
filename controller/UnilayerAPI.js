0f40f2e502f32913bdeeeb090def576f05109170
Sagar-Eiysys <sagar@eiysys.in>
2020-09-04 15:00:52 +0530

    Updated code

--- controller/UnilayerAPI.js
+++ controller/UnilayerAPI.js
@@ -1,14 +1,54 @@
 
-var express = require('express');
-var config = require('../routes/config.json');
+const express = require('express');
+const Web3 = require("web3");
+const config = require('../routes/config.json');
 const axios = require('axios');
+//const DEXAG = require('dexag-sdk');
+//import {DEXAG} from 'dexag-sdk'
+//const sdk = DEXAG.fromProvider(window.ethereum);
 
 
 module.exports = {
+
+    // getPairExchangeDetailByAddress: async (req, res) => {
+        
+    //     if (req.query.exchangeAddress && !req.query.exchangeAddress == "") {
+    //         console.log(req.query.exchangeAddress)
+    //         await axios.get('https://data-api.defipulse.com/api/v1/blocklytics/pools/v1/exchange/'+req.query.exchangeAddress).then(async output=>{
+    //         let response = {status:true, data:output.data};
+    //         res.send(response);            
+    //         }).catch(err => {
+    //             console.log(err)
+    //             let response = {status:false, message:"Unable to get Pair Exchange Detail by Address, Please Try Again!!!"};
+    //             res.send(response);
+    //         });
+    //     } else {
+    //         let response = {status:false, message:"Enter valid Exchange Address & Try Again!!!"};
+    //         res.send(response);
+    //     }
+    // },
+
+    // getAllPairExchangeDetail: async (req, res) => {
+        
+    //    // if (req.query.platform && !req.query.platform == "") {
+
+    //         await axios.get('https://data-api.defipulse.com/api/v1/blocklytics/pools/v1/exchanges').then(async output=>{
+    //         let response = {status:true, data:output.data};
+    //         res.send(response);            
+    //         }).catch(err => {
+    //             console.log(err)
+    //             let response = {status:false, message:"Unable to get Pair Price Detail, Please Try Again!!!"};
+    //             res.send(response);
+    //         });
+    //     // } else {
+    //     //     let response = {status:false, message:"Enter valid Entity & Try Again!!!"};
+    //     //     res.send(response);
+    //     // }
+    // },
  
     getPairPriceDetail: async (req, res) => {
         
-        if (req.query.from && req.query.to && req.query.from == "" && !req.query.to == "") {
+        if (req.query.from && req.query.to && !req.query.from == "" && !req.query.to == "") {
             await axios.get('https://api-v2.dex.ag/price?from='+req.query.from+'&to='+req.query.to+'&fromAmount='+req.query.fromAmount+'&dex=uniswap').then(async output=>{
             let response = {status:true, data:output.data};
             res.send(response);            
@@ -24,7 +64,7 @@ module.exports = {
 
     getPairTradeDetail: async (req, res) => {
         
-        if (req.query.from && req.query.to && req.query.from == "" && !req.query.to == "") {
+        if (req.query.from && req.query.to && !req.query.from == "" && !req.query.to == "") {
             await axios.get('https://api-v2.dex.ag/trade?from='+req.query.from+'&to='+req.query.to+'&fromAmount='+req.query.fromAmount+'&dex=uniswap').then(async output=>{
             let response = {status:true, data:output.data};
             res.send(response);            
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,31 @@ const router = express.Router();
 const unilayerApi = require('../controller/UnilayerAPI');
 
 
+
+//     /**
+//     * @typedef getPairExchangeDetailByAddress
+//     * @property {String} exchangeAddress.required - Add exchangeAddress - eg: qwertyuikjhgfdsdfvbn
+//     */
+//     /**
+//     * @route GET /api/uniswap/getPairExchangeDetailByAddress
+//     * @param {getPairExchangeDetailByAddress.model} exchangeAddress.query
+//     * @group Generic_API
+//     * @security Basic Auth
+//     */
+// router.get('/getPairExchangeDetailByAddress', unilayerApi.getPairExchangeDetailByAddress);
+
+//     /**
+//     * @typedef getAllPairExchangeDetail
+//     * @property {String} platform.required - Add platform - eg: uniswap-v2
+//     */
+//     /**
+//     * @route GET /api/uniswap/getAllPairExchangeDetail
+//     * @param {getAllPairExchangeDetail.model} platform.query
+//     * @group Generic_API
+//     * @security Basic Auth
+//     */
+// router.get('/getAllPairExchangeDetail', unilayerApi.getAllPairExchangeDetail);
+
    /**
     * @typedef getPairPriceDetail
     * @property {String} from.required - Add from - eg: DAI
