20a83d0873897ae1989102390011165cb4e40f1b
Sagar-Eiysys <sagar@eiysys.in>
2020-09-08 10:32:44 +0530

    Upadated code with new api

--- controller/UnilayerAPI.js
+++ controller/UnilayerAPI.js
@@ -8,6 +8,11 @@ var apiKey = "e5bc8349129fe611931adedb2c6835f3500ba08cbcfb3ba24a01db648614";
 
 module.exports = {
 
+
+    /**
+    * @dev get owner of particular token exchange address
+    * @return address of owner account with balance
+    */
     getOwnerByExchangeAddress: async (req, res) => {
         
         if (req.query.exchangeAddress && !req.query.exchangeAddress == "") {
@@ -25,6 +30,10 @@ module.exports = {
         }
     },
 
+    /**
+    * @dev get token pair detail by exchange address
+    * @return token pair detail with token address , symbol and balance
+    */
     getPairDetailByExchangeAddress: async (req, res) => {
         
         if (req.query.exchangeAddress && !req.query.exchangeAddress == "") {
@@ -42,6 +51,10 @@ module.exports = {
         }
     },
 
+    /**
+    * @dev get token pair trade detail by exchange address
+    * @return token pair trade detail with price, id, timestamp and other important detail
+    */
     getTradeDetailByExchangeAddress: async (req, res) => {
         
         if (req.query.exchangeAddress && !req.query.exchangeAddress == "") {
@@ -59,6 +72,10 @@ module.exports = {
         }
     },
 
+    /**
+    * @dev get return and share detail by exchange address
+    * @return exchange address shares and roi details
+    */
     getReturnSharesDetailByExchangeAddress: async (req, res) => {
         
         if (req.query.exchangeAddress && !req.query.exchangeAddress == "") {
@@ -76,6 +93,10 @@ module.exports = {
         }
     },
  
+    /**
+    * @dev get price of token pair 
+    * @return pair price detail on basis of input token symbol 
+    */
     getPairPriceDetail: async (req, res) => {
         
         if (req.query.from && req.query.to && !req.query.from == "" && !req.query.to == "") {
@@ -92,6 +113,10 @@ module.exports = {
         }
     },
 
+    /**
+    * @dev get token pair trade detail 
+    * @return pair trade details having transaction time and important other details too 
+    */
     getPairTradeDetail: async (req, res) => {
         
         if (req.query.from && req.query.to && !req.query.from == "" && !req.query.to == "") {
@@ -108,6 +133,10 @@ module.exports = {
         }
     },
 
+    /**
+    * @dev get all token pair exchange detail 
+    * @return exchange address with token detail and symbol 
+    */
     getAllPairExchangeDetail: async (req, res) => {
         
         await axios.get('https://data-api.defipulse.com/api/v1/blocklytics/pools/v1/exchanges?api-key='+apiKey+'&platform=Uniswap-v2').then(async output=>{
@@ -119,6 +148,10 @@ module.exports = {
         });
     },
 
+    /**
+    * @dev get all available token pair detail 
+    * @return all token pair with their exchange address and symbol 
+    */
     getAllAvailableTokenPairs : async (req, res) => {
 
             await axios.get('https://api-v2.dex.ag/token-list-full').then(async output=>{
